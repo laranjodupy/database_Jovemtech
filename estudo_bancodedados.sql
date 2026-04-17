fb1ae9dab13cea9b776f20058d145526c9b2faa6
laranjodupy <vianalimadaniel3@gmail.com>
2026-04-17 09:54:53 -0300

    alterando o código pois o anterior era apenas de teste e introdução (esse também, só que mais direcionado)

--- estudo_bancodedados.sql
+++ estudo_bancodedados.sql
@@ -1,3 +1,24 @@
-select * from "tack" 
+CREATE TABLE IF NOT EXISTS clientes(
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  status VARCHAR(20) DEFAULT 'ativo',
+  limite NUMERIC(10,2) CHECK (LIMITE >=0),
+  criado_em TIMESTAMPTZ DEFAULT NOW()
+);
 
-/*A coluna album_id se liga esta na tabela playlist_track e que se liga à tabela playlist, assim interconectando estas 3 tabelas*/
+-- Tabela pai
+CREATE TABLE IF NOT EXISTS autores (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT null
+);
+
+-- Depois a tabela filha (que faz a referência)
+CREATE TABLE IF NOT EXISTS livros(
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(150) NOT NULL,
+  preco NUMERIC(10,2) NOT NULL,
+  autor_id INTEGER REFERENCES autores(id) ON DELETE RESTRICT
+);
+
+ALTER TABLE livros ADD COLUMN paginas INTEGER;
